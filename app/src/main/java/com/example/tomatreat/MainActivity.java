fe5439a96e2c77afff05f12e5cc78bf9ef356599
Muhammad Fachry Ramadhan <78468976+fachrydan@users.noreply.github.com>
2021-06-03 02:57:19 +0700

    Update MainActivity.java

--- app/src/main/java/com/example/tomatreat/MainActivity.java
+++ app/src/main/java/com/example/tomatreat/MainActivity.java
@@ -6,11 +6,46 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    Button btOpen;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        imageView = findViewById(R.id.image_leaf1);
+        //btOpen = findViewById(R.id.button_camera);
+        FloatingActionButton btOpen = (FloatingActionButton) findViewById(R.id.button_camera);
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{
+                            Manifest.permission.CAMERA
+                    },
+                    100);
+        }
+
+        btOpen.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 100);
+            }
+        });
+    }
+    
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 100) {
+
+            //Get Capture Image
+            Bitmap captureImage = (Bitmap) data.getExtras().get("data");
+            //Set Capture
+            imageView.setImageBitmap(captureImage);
+
+        }
     }
 }
